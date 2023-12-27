aa992a3ab23336cfe16e38da29d9d8477dede423
Ilya <kulak1224@gmail.com>
2023-12-27 14:45:12 +0200

    2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -33,7 +33,7 @@ const errorToastConfig = {
   backgroundColor: '#EF4040',
   titleColor: '#ffffff',
   messageColor: '#ffffff',
-  iconUrl: '../img/icon-error.svg',
+  iconUrl: './img/icon-error.svg',
 };
 
 function onStartClick() {
