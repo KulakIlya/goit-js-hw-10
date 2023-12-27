98c55835f099d545472f0b0323ef6f6c6debcc18
Ilya <kulak1224@gmail.com>
2023-12-27 14:32:47 +0200

    refactoring

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,14 +3,9 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import { onStartClick } from './helpers';
 
 const startBtn = document.querySelector('.btn');
-const timerValues = {
-  days: document.querySelector('.value[data-days]'),
-  hours: document.querySelector('.value[data-hours]'),
-  minutes: document.querySelector('.value[data-minutes]'),
-  seconds: document.querySelector('.value[data-seconds]'),
-};
 
 let selectedDate;
 
@@ -30,7 +25,6 @@ const flatpickrConfig = {
     startBtn.removeAttribute('disabled');
   },
 };
-
 flatpickr('#datetime-picker', flatpickrConfig);
 
 const errorToastConfig = {
@@ -43,44 +37,6 @@ const errorToastConfig = {
   iconUrl: '../img/icon-error.svg',
 };
 
-function updateTime() {
-  const { days, hours, minutes, seconds } = convertMs(
-    selectedDate - Date.now()
-  );
-
-  console.log(timerValues.seconds.value);
-
-  timerValues.days.textContent = days;
-  timerValues.hours.textContent = hours;
-  timerValues.minutes.textContent = minutes;
-  timerValues.seconds.textContent = seconds;
-}
-
 function onStartClick() {
   const interval = setInterval(updateTime, 1000);
-}
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = addingLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addingLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addingLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addingLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
-
-  return { days, hours, minutes, seconds };
-}
-
-function addingLeadingZero(value) {
-  return String(value).padStart(2, '0');
 }
---
+++ src/js/helpers.js
@@ -0,0 +1,42 @@
+const timerValues = {
+  days: document.querySelector('.value[data-days]'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
+};
+
+export function updateTime() {
+  const { days, hours, minutes, seconds } = convertMs(
+    selectedDate - Date.now()
+  );
+
+  timerValues.days.textContent = days;
+  timerValues.hours.textContent = hours;
+  timerValues.minutes.textContent = minutes;
+  timerValues.seconds.textContent = seconds;
+}
+
+export function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addingLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addingLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addingLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addingLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+export function addingLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
